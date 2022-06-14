b6ddb1829caf67b6e58ab36fc8396a9a735ad32e
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-06-14 16:39:37 +0200

    Solved assignment 1

---
+++ assign1.php
@@ -0,0 +1,19 @@
+<form action="" method="get">
+    <input type="text" placeholder="What is Your Age?" name="age">
+    <input type="submit" value="GO" name="submit">
+</form>
+
+    <?php 
+    if (isset($_GET['submit'])) {
+        $age = $_GET['age'];
+        if (! is_numeric($age)) {
+            echo "This is Text please try again" ; 
+            die() ; 
+        }
+        if ($age >= 18) {
+            echo "You are old enough to vote!";
+        } else {
+            echo "You are not old enough to vote!";
+        }
+    }
+    ?>
