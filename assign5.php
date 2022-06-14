896b37a55926f01b1b84ec78151c6a60912acb68
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-06-14 17:03:59 +0200

    Another assignment 5 Solution

--- assign5.php
+++ assign5.php
@@ -9,3 +9,17 @@ for($i = 0  ; $i < count($babel_List) ;  $i++) {
     }
     // This will ONLY show the NON-Boolean Values ; 
 }
+
+
+// In Case You Want it using the While Loop ; 
+
+// <?php
+// $babel_List  = ["Ayman" , 'A' , 1 , true , 123 , "soso" , false ,true ]  ;
+// $start  =  0 ;  
+// while ($start < count($babel_List)) {
+//     if(!is_bool($babel_List[$start])) {
+//         echo $babel_List[$start] . "<br>" ;
+//     }
+//     $start++ ;
+// }
+// ?>
