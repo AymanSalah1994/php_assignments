0b09f51fc7dea432d3913110376fd0da376f02dd
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-06-14 16:54:56 +0200

    Assignment #3

---
+++ assign3.php
@@ -0,0 +1,13 @@
+<?php 
+// Find the Max Number  ; 
+
+$numbers  = array(1,2,3,4,5,6,7,8,9,10);
+// $numbers = [1,2,3,4,5,6,7,8,9,10];
+
+$max  =  0  ;
+foreach ($numbers as $number) {
+    if ($number > $max) {
+        $max = $number;
+    }
+}
+echo "Max Number is : " . $max;
